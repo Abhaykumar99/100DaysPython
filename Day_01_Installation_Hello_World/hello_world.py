dd0cb5e12feeac6bde03b3e1cc2cf243679fb2e2
Abhay Kumar <krabhi4201@gmail.com>
2025-01-01 14:42:02 +0530

    commit

---
+++ Day_01_Installation_Hello_World/hello_world.py
@@ -0,0 +1,12 @@
+# ============================================================
+# Day 1: Installation & Hello World
+# ============================================================
+# Topics: Python setup, VS Code setup, writing your first program
+# ============================================================
+
+# Your very first Python program!
+print("Hello, World!")
+
+# You can also print other things:
+print("Welcome to 100 Days of Python!")
+print("Let's get started!")
